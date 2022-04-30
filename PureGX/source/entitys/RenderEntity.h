e4726af264d68eda1740659de2d5b812f96b2f11
Austin Harms <austin@harmsonline.com>
2022-04-30 14:25:51 -0500

    Remove RenderEntity debug code

--- PureGX/source/entitys/RenderEntity.h
+++ PureGX/source/entitys/RenderEntity.h
@@ -14,33 +14,6 @@ public:
 
 	void onUpdate(Mtx& matrix) override {
 		Renderer::getInstance().drawRenderMesh(_renderMesh, matrix);
-		GX_Begin(GX_TRIANGLES, GX_VTXFMT0, 6);
-		Vector3f pos(0);
-		GX_Position3f32(0 + pos.x, 0 + pos.y, 0 + pos.z);
-		GX_Color1u32(0xffffffff);
-		GX_TexCoord2f32(0.0f, 0.0f);
-
-		GX_Position3f32(1 + pos.x, 1 + pos.y, 0 + pos.z);
-		GX_Color1u32(0xffffffff);
-		GX_TexCoord2f32(1.0f, 1.0f);
-
-		GX_Position3f32(0 + pos.x, 1 + pos.y, 0 + pos.z);
-		GX_Color1u32(0xffffffff);
-		GX_TexCoord2f32(0.0f, 1.0f);
-
-		GX_Position3f32(0 + pos.x, 0 + pos.y, 0 + pos.z);
-		GX_Color1u32(0xffffffff);
-		GX_TexCoord2f32(0.0f, 0.0f);
-
-		GX_Position3f32(1 + pos.x, 0 + pos.y, 0 + pos.z);
-		GX_Color1u32(0xffffffff);
-		GX_TexCoord2f32(1.0f, 0.0f);
-
-		GX_Position3f32(1 + pos.x, 1 + pos.y, 0 + pos.z);
-		GX_Color1u32(0xffffffff);
-		GX_TexCoord2f32(1.0f, 1.0f);
-
-		GX_End();
 	}
 
 private:
